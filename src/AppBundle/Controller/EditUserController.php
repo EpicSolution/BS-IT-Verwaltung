c9af7da8b5d5d485eaa1f3b1d48cfaceda1aa733
Vladislav Sultanov <sultanov.vladislav23@googlemail.com>
2018-07-23 19:55:56 +0200

    Benutzer bearbeiten Seite erstellt und die url von dem button in user_list.html.twig geändert

--- src/AppBundle/Controller/EditUserController.php
+++ src/AppBundle/Controller/EditUserController.php
@@ -10,7 +10,15 @@ declare(strict_types=1);
 namespace AppBundle\Controller;
 
 
+use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,9 +27,40 @@ class EditUserController extends Controller
     /**
      * @Route("/edit/user/{id}", name="edit_user", requirements  = { "id" = "\d+" })
      */
-    public function editAction($id)
+    public function editAction(Request $request, string $id)
     {
-        dump($id);
-        die();
+        $user = $this->getDoctrine()->getRepository(User::class)->find($id);
+        $form = $this->getForm($user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $userManager = $this->get('fos_user.user_manager');
+            $userManager->updateUser($user);
+        }
+        
+        return $this->render('user/edit_user.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    private function getForm(User $user): FormInterface
+    {
+        $form = $this->createFormBuilder($user)
+            ->add('username', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('plainPassword', PasswordType::class)
+            ->add('roles', ChoiceType::class, [
+                'multiple' => true,
+                'choices' => [
+                    'Systembetreuer' => 'ROLE_SYSTEM_ADMINISTRATOR',
+                    'Azubi' => 'ROLE_TRAINEE',
+                    'Verwalter' => 'ROLE_ADMINISTRATOR',
+                    'Lehrer' => 'ROLE_TEACHER'
+                ],
+            ])
+            ->add('submit', SubmitType::class)
+            ->getForm();
+
+        return $form;
     }
 }
