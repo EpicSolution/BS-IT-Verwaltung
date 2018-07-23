2708c71e4754af7652bfdc6edcf01769458ed740
Vladislav Sultanov <sultanov.vladislav23@googlemail.com>
2018-07-23 13:48:24 +0200

    User Delete controller hinzugefügt

---
+++ src/AppBundle/Controller/deleteUserController.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * Created by solutionDrive GmbH
+ *
+ * @copyright 2018 solutionDrive GmbH
+ */
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class deleteUserController extends Controller
+{
+    public function deleteUserAction()
+    {
+
+    }
+}
