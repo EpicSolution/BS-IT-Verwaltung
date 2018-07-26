2cab6f64a57d4a24756a9f2728ce6915971a2291
Vladislav Sultanov <sultanov.vladislav23@googlemail.com>
2018-07-26 07:53:16 +0200

    Request bearbeiten

--- src/AppBundle/Controller/AddComponentTypeController.php
+++ src/AppBundle/Controller/AddComponentTypeController.php
@@ -10,14 +10,21 @@ declare(strict_types=1);
 namespace AppBundle\Controller;
 
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class AddComponentTypeController extends Controller
 {
+    /**
+     * @Route("/add/component_type", name="add_component_type")
+     */
     public function addComponentTypeAction(Request $request)
     {
+        if ($request->getMethod() === 'POST') {
+            $content = $request->request->all();
 
+        }
         return $this->render('componentType/add_component_type.html.twig');
     }
 }
